0bf51484ffbd1155374478bf66824d37f043f965
marina101 <mchirc1@gmail.com>
2016-03-05 22:59:39 -0500

    added comments to server file

--- simple_server.rb
+++ simple_server.rb
@@ -1,10 +1,13 @@
-require 'socket'               
+require 'socket'            
 require 'json'
 
 server = TCPServer.open(2000)  # Socket to listen on port 2000
 loop {                         # Servers run the whole time
-  socket = server.accept        # Wait for a client to connect, returns TCP socket representing connection once it gets one
+  socket = server.accept      # Wait for a client to connect, returns TCP
+                               # socket representing connection once it gets one
   
+
+  # Reads the header of incoming http request
   lines = []
   header = ""
   while 
@@ -15,10 +18,10 @@ loop {                         # Servers run the whole time
   end 
   STDERR.puts header.to_s 
 
+  # Assembles http response header and message based on request type
   response = ""
   tag = ""
   elements = lines[0].split(" ")
-
   filename = elements[1]
 
   if File.exist?(filename)
@@ -34,13 +37,14 @@ loop {                         # Servers run the whole time
       body = ""
       # parse out the size, in bytes, of the request body from the header
       size_line = lines[2].split(" ")
-      body_size = size_line[1].to_i
-      body = socket.read(body_size.to_i)
+      body_size = size_line[1].to_i  
+      body = socket.read(body_size.to_i)  #read the body of the message
 
-
+      #parses json object and adds submitted content into original html file
       params = JSON.parse(body)
       
-      people = "<li>name: #{params['person']['name']}</li><li>e-mail: #{params['person']['email']}</li>"
+      people = "<li>name: #{params['person']['name']}</li><li>e-mail: " +
+        "#{params['person']['email']}</li>"
       modified_file = file_contents.gsub("<%= yield %>", people) 
       response = modified_file
       tag = "200 OK"
@@ -52,7 +56,9 @@ loop {                         # Servers run the whole time
     response = "404 Error, your file does not exit."
     tag = "404 File_not_found"
   end
-    
+  
+  # Sends response back to browser:
+
   socket.print "HTTP/1.1 #{tag}\r\n" +
                "Content-Type: text/plain\r\n" +
                "Content-Length: #{response.bytesize}\r\n" +
@@ -65,11 +71,4 @@ loop {                         # Servers run the whole time
   socket.print response 
   
   socket.close                 # Disconnect from the socket
-
-  
-
-
-
-  
- 
-}  
+} 
